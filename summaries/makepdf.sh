c22d519c2735c46bd891818ac86cb2e7a19776bc
crstjean <crstjean@gmail.com>
2013-10-01 18:28:00 -0400

    put the proposal into unh format

--- summaries/makepdf.sh
+++ summaries/makepdf.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
-
+dviEnding=".dvi"
+psEnding=".ps"
 latex $1
 bibtex $1
 latex $1
 latex $1
-dvips -Ppdf $1
-dvipdf $1
+dvips -t a4 $1$dviEnding
+ps2pdf -sPAPERSIZE=a4 $1$psEnding
+#dvips -Ppdf $1
+#dvipdf $1
